f11bc3de217c8a1b23fb8a186c25bfd70005441e
kkeeratis <148253349+kkeeratis@users.noreply.github.com>
2024-02-02 15:35:02 +0000

    Create table and insert info

--- 01-Data modeling-i/create_tables.py
+++ 01-Data modeling-i/create_tables.py
@@ -1,36 +1,86 @@
+import logging
 from typing import NewType
 
 import psycopg2
+import traceback
 
 PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
 PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
-table_drop_events = "DROP TABLE IF EXISTS events"
-table_drop_actors = "DROP TABLE IF EXISTS actors"
+# Queries to drop tables if they exist
+table_drop_events = "DROP TABLE IF EXISTS events CASCADE"
+table_drop_actors = "DROP TABLE IF EXISTS actors CASCADE"
+table_drop_reactions = "DROP TABLE IF EXISTS reactions CASCADE"
+table_drop_state = "DROP TABLE IF EXISTS state CASCADE"
+table_drop_comments = "DROP TABLE IF EXISTS comments CASCADE"  # เพิ่มตาราง comments ในการลบ
 
+# Queries to create tables
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
-        id serial PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         login text
     )
 """
 
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
-        id serial PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         type text,
-        actor_id int REFERENCES actors(id),
-        labels text,  -- Added 'labels' column
-        milestone text  -- Added 'milestone' column
+        actor_id bigint,  -- เปลี่ยนชนิดข้อมูลเป็น bigint
+        FOREIGN KEY (actor_id) REFERENCES actors(id)
     )
 """
 
+table_create_reactions = """
+    CREATE TABLE IF NOT EXISTS reactions (
+        id SERIAL PRIMARY KEY,
+        event_id bigint, 
+        reaction_type text,
+        user_id int,
+        created_at timestamp,
+        FOREIGN KEY (event_id) REFERENCES events(id),
+        FOREIGN KEY (user_id) REFERENCES actors(id)
+    )
+"""
+
+table_create_state = """
+    CREATE TABLE IF NOT EXISTS state (
+        id SERIAL PRIMARY KEY,
+        event_id bigint,  -- เปลี่ยนชนิดข้อมูลเป็น bigint
+        state_type text,
+        user_id int,
+        created_at timestamp,
+        FOREIGN KEY (event_id) REFERENCES events(id),
+        FOREIGN KEY (user_id) REFERENCES actors(id)
+    )
+"""
+
+# New table for comments
+table_create_comments = """
+    CREATE TABLE IF NOT EXISTS comments (
+        id SERIAL PRIMARY KEY,
+        event_id bigint,  -- เปลี่ยนชนิดข้อมูลเป็น bigint
+        user_id int,
+        comment_text text,
+        created_at timestamp,
+        FOREIGN KEY (event_id) REFERENCES events(id),
+        FOREIGN KEY (user_id) REFERENCES actors(id)
+    )
+"""
+
+# Lists of queries for dropping and creating tables
 create_table_queries = [
     table_create_actors,
     table_create_events,
+    table_create_reactions,
+    table_create_state,
+    table_create_comments,
 ]
 
 drop_table_queries = [
+    table_drop_reactions,
+    table_drop_state,
+    table_drop_comments,  # เพิ่มตาราง comments ในการลบ
     table_drop_events,
     table_drop_actors,
 ]
@@ -55,20 +105,25 @@ def main():
     """
     - Drops (if exists) and Creates the sparkify database.
     - Establishes connection with the sparkify database and gets
-    cursor to it.
+      cursor to it.
     - Drops all the tables.
     - Creates all tables needed.
     - Finally, closes the connection.
     """
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
-    )
-    cur = conn.cursor()
-
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
-
-    conn.close()
+    try:
+        conn = psycopg2.connect(
+            "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
+        )
+        with conn:
+            cur = conn.cursor()
+            drop_tables(cur, conn)
+            create_tables(cur, conn)
+    except psycopg2.Error as e:
+        logging.error(f"An error occurred: {e}")
+        traceback.print_exc()
+    finally:
+        if conn is not None:
+            conn.close()
 
 if __name__ == "__main__":
     main()
--- 01-Data modeling-i/etl.py
+++ 01-Data modeling-i/etl.py
@@ -5,7 +5,6 @@ from typing import List
 
 import psycopg2
 
-
 def get_files(filepath: str) -> List[str]:
     """
     Description: This function is responsible for listing the files in a directory
@@ -21,6 +20,36 @@ def get_files(filepath: str) -> List[str]:
 
     return all_files
 
+def insert_actor(cur, each):
+    actor_insert_statement = """
+        INSERT INTO actors (id, login)
+        VALUES (%s, %s)
+        ON CONFLICT (id) DO NOTHING
+    """
+    cur.execute(actor_insert_statement, (each["actor"]["id"], each["actor"]["login"]))
+
+def insert_event(cur, each):
+    event_insert_statement = """
+        INSERT INTO events (id, type, actor_id)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (id) DO NOTHING
+    """
+    cur.execute(event_insert_statement, (str(each["id"]), each["type"], each["actor"]["id"]))
+
+def insert_comment(cur, each):
+    if each["type"] == "IssueCommentEvent":
+        comment_insert_statement = """
+            INSERT INTO comments (id, event_id, user_id, comment_text, created_at)
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (id) DO NOTHING
+        """
+        cur.execute(comment_insert_statement, (
+            each["id"],
+            each["id"],
+            each["actor"]["id"],
+            each["payload"]["comment"]["body"],
+            each["created_at"]
+        ))
 
 def process(cur, conn, filepath):
     # Get list of files from filepath
@@ -31,62 +60,114 @@ def process(cur, conn, filepath):
             data = json.loads(f.read())
             for each in data:
                 # Print some sample data
-                print(
-                    each["id"],
-                    each["type"],
-                    each["actor"]["id"],
-                    each["actor"]["login"],
-                    each["repo"]["id"],
-                    each["repo"]["name"],
-                    each["created_at"],
-                    each["payload"]["issue"]["url"] if each["type"] == "IssueCommentEvent" else None,
-                )
+                if each["type"] == "IssueCommentEvent":
+                    print(
+                        each["id"],
+                        each["type"],
+                        each["actor"]["id"],
+                        each["actor"]["login"],
+                        each["repo"]["id"],
+                        each["repo"]["name"],
+                        each["created_at"],
+                        each["payload"]["issue"]["url"],
+                    )
+                else:
+                    print(
+                        each["id"],
+                        each["type"],
+                        each["actor"]["id"],
+                        each["actor"]["login"],
+                        each["repo"]["id"],
+                        each["repo"]["name"],
+                        each["created_at"],
+                    )
 
-                # Insert data into tables here
-                actor_insert_statement = f"""
-                    INSERT INTO actors (
-                        id,
-                        login
-                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
-                    ON CONFLICT (id) DO NOTHING
-                """
-                cur.execute(actor_insert_statement)
-
-                # Format labels and milestone as JSON strings
-                labels_str = json.dumps(each.get("payload", {}).get("issue", {}).get("labels", []))
-                milestone_str = json.dumps(each.get("payload", {}).get("issue", {}).get("milestone", ""))
-
-                event_insert_statement = f"""
-                    INSERT INTO events (
-                        id,
-                        type,
-                        actor_id,
-                        labels,
-                        milestone
-                    ) VALUES ('{each["id"]}', '{each["type"]}', {each["actor"]["id"]},
-                               '{labels_str}', '{milestone_str}')
-                    ON CONFLICT (id) DO NOTHING
-                """
-                cur.execute(event_insert_statement)
+                # Insert data into tables
+                insert_actor(cur, each)
+                insert_event(cur, each)
+                insert_comment(cur, each)
 
                 conn.commit()
 
+def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
+    table_drop_comments = "DROP TABLE IF EXISTS comments CASCADE"
+    table_drop_reactions = "DROP TABLE IF EXISTS reactions CASCADE"
+    table_drop_state = "DROP TABLE IF EXISTS state CASCADE"
+    table_drop_events = "DROP TABLE IF EXISTS events CASCADE"
+    table_drop_actors = "DROP TABLE IF EXISTS actors CASCADE"
 
-def etl_process(cur, conn, filepath):
-    # Perform ETL for all files in the specified directory
-    process(cur, conn, filepath)
+    drop_table_queries = [
+        table_drop_comments,
+        table_drop_reactions,
+        table_drop_state,
+        table_drop_events,
+        table_drop_actors,
+    ]
 
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list.
+    """
+    table_create_actors = """
+        CREATE TABLE IF NOT EXISTS actors (
+            id bigint PRIMARY KEY,
+            login text
+        )
+    """
+
+    table_create_events = """
+        CREATE TABLE IF NOT EXISTS events (
+            id bigint PRIMARY KEY,
+            type text,
+            actor_id bigint,
+            FOREIGN KEY (actor_id) REFERENCES actors(id)
+        )
+    """
+
+    table_create_comments = """
+        CREATE TABLE IF NOT EXISTS comments (
+            id bigint PRIMARY KEY,
+            event_id bigint,
+            user_id bigint,
+            comment_text text,
+            created_at timestamp,
+            FOREIGN KEY (event_id) REFERENCES events(id),
+            FOREIGN KEY (user_id) REFERENCES actors(id)
+        )
+    """
+
+    create_table_queries = [
+        table_create_actors,
+        table_create_events,
+        table_create_comments,
+    ]
+
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
 
 def main():
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
-    )
-    cur = conn.cursor()
-
-    etl_process(cur, conn, filepath="../data")
-
-    conn.close()
-
+    try:
+        conn = psycopg2.connect(
+            "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
+        )
+        with conn:
+            cur = conn.cursor()
+            drop_tables(cur, conn)
+            create_tables(cur, conn)
+            process(cur, conn, filepath="../data")
+    except psycopg2.Error as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if conn is not None:
+            conn.close()
 
 if __name__ == "__main__":
     main()
