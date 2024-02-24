700db047f23c9d00196d0c87e811f66b73204df9
kkeeratis <148253349+kkeeratis@users.noreply.github.com>
2024-02-24 16:18:38 +0000

    Load data into GG bigquery

--- 03-building-a-data-warehouse/etl_bigquery.py
+++ 03-building-a-data-warehouse/etl_bigquery.py
@@ -36,13 +36,13 @@ def main(dataset_id, table_id, file_path):
     # keyfile = os.environ.get("KEYFILE_PATH")
     #
     # แต่เพื่อความง่ายเราสามารถกำหนด File Path ไปได้เลยตรง ๆ
-    keyfile = "YOUR_KEYFILE_PATH"
+    keyfile = "ds525-load-data-to-bigquary.json"
     service_account_info = json.load(open(keyfile))
     credentials = service_account.Credentials.from_service_account_info(service_account_info)
 
     # โค้ดส่วนนี้จะเป็นการสร้าง Client เชื่อมต่อไปยังโปรเจค GCP ของเรา โดยใช้ Credentials ที่
     # สร้างจากโค้ดข้างต้น
-    project_id = "YOUR_GCP_PROJECT"
+    project_id = "building-a-data-warehouse"
     client = bigquery.Client(
         project=project_id,
         credentials=credentials,
@@ -57,6 +57,9 @@ def main(dataset_id, table_id, file_path):
         schema=[
             bigquery.SchemaField("id", bigquery.SqlTypeNames.STRING),
             bigquery.SchemaField("type", bigquery.SqlTypeNames.STRING),
+            bigquery.SchemaField("actor", bigquery.SqlTypeNames.STRING),
+            bigquery.SchemaField("repo", bigquery.SqlTypeNames.STRING),
+            bigquery.SchemaField("created_at", bigquery.SqlTypeNames.STRING)
         ],
     )
 
@@ -78,11 +81,12 @@ if __name__ == "__main__":
 
     with open("github_events.csv", "w") as csv_file:
         writer = csv.writer(csv_file)
+        writer.writerow(["id", "type", "actor", "repo", "created_at"])  # แก้ไขตรงนี้
 
         for datafile in all_files:
             with open(datafile, "r") as f:
                 data = json.loads(f.read())
                 for each in data:
-                    writer.writerow([each["id"], each["type"]])
+                    writer.writerow([each["id"], each["type"], each["actor"]["login"], each["repo"]["name"], each["created_at"]])  # แก้ไขตรงนี้
 
-    main(dataset_id, table_id, file_path)
+    main(dataset_id="GitHub", table_id="events", file_path="github_events.csv")  # แก้ไขตรงนี้
